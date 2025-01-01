e40a0d341fcd861bf39db7835782ab0536850add
Dan Sullivan <dansullivan@gmail.com>
2025-01-01 17:57:31 -0500

    test: output of function call

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,24 @@
+fn get_pid_message() -> String {
+    format!("Hello. My PID is {}", std::process::id())
+}
+
 fn main() {
-    println!("Hello. My PID is {}", std::process::id());
+    println!("{}", get_pid_message());
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_main() {
-        super::main();
+        main();
+    }
+
+    #[test]
+    fn test_get_pid_message() {
+        let message = get_pid_message();
+        assert!(message.starts_with("Hello. My PID is "));
+        assert!(message.len() > 14); // Basic length check to ensure PID is included
     }
 }
