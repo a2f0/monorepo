59872886de3a4714b5b4c083f899e9be65bf06e8
Dan Sullivan <dansullivan@gmail.com>
2025-01-01 18:06:11 -0500

    refactor: move tests to separate file

--- hello/src/tests/main_test.rs
+++ hello/src/tests/main_test.rs
@@ -1,10 +1,4 @@
-pub fn get_pid_message() -> String {
-    format!("Hello. My PID is {}", std::process::id())
-}
-
-pub fn main() {
-    println!("{}", get_pid_message());
-}
+use hello;
 
 #[cfg(test)]
 mod tests {
