23fd93eac0be72a2e8cbc35caaa720106b1b29a1
Dan Sullivan <dansullivan@gmail.com>
2025-01-04 18:38:53 -0500

    cleanup: duplicate test code

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,20 +5,3 @@ pub fn get_pid_message() -> String {
 pub fn main() {
     println!("{}", get_pid_message());
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_main() {
-        main();
-    }
-
-    #[test]
-    fn test_get_pid_message() {
-        let message = get_pid_message();
-        assert!(message.starts_with("Hello. My PID is "));
-        assert!(message.len() > 14); // Basic length check to ensure PID is included
-    }
-}
